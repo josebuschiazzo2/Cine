62798e2069167b5d44269a417bccf0b5cacf4b57
José Buschiazzo <104038315+josebuschiazzo2@users.noreply.github.com>
2022-05-29 13:21:20 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,55 @@
-import "./styles.css";
+/* Diseñar un algoritmo que recorra las butacas de una sala de cine y determine cuántas butacas desocupadas hay
+• Suponga que para modelar este problema, se utiliza un arreglo con valores lógicos
+• La presencia de un valor verdadero (true) en el arreglo indica que la butaca está ocupada
+• La presencia de un valor falso (false) en el arreglo indica que la butaca está desocupada*/
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+//Empiezo con el main
+console.clear();
+let seatsunoccupied:number=0;
+let seats:boolean[][]=[ 
+  [true, false , true],
+  [true, false , true],
+  [true, false , true]
+  ];
+
+  for (let x:number=0;x<3;x++) 
+  {
+    for (let y:number=0;y<3;y++)
+    {
+      if (!(seats[x][y])) 
+      {
+        seatsunoccupied++;
+      }
+    }
+  }
+
+
+console.log("La cantidad de asientos desocupados son: " + seatsunoccupied);
+
+// voy a probar haciendo un random
+let numrandom:number[][]=new Array(3);
+seatsunoccupied=0;  
+for (let x:number=0;x<3;x++) 
+  {
+    for (let y:number=0;y<3;y++)
+    {
+      numrandom[x][y]=((Math.random().toFixed(1)*10)%2);
+      console.log(numrandom[x][y]);
+      if (!(seats[x][y])) 
+      {
+        seatsunoccupied++;
+      }
+    }
+  }
+for (let x:number=0;x<3;x++) 
+  {
+    for (let y:number=0;y<3;y++)
+    {
+      if (!(seats[x][y])) 
+      {
+        seatsunoccupied++;
+      }
+    }
+  }
+  
+console.log("La cantidad de asientos desocupados son: " + seatsunoccupied);
